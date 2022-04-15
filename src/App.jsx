dfeb930c396c63dc8bb10798e37c360b12296497
DmitriyAfa <dmitriy.olegovich.a.1996@gmail.com>
2022-04-15 06:10:00 +0500

    AppHeader

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import {Box, Logo, BurgerIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+import AppHeader from './components/app-header/app-header';
+
+
+function App() {
+  return (
+    <>
+    <AppHeader />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {Box, Logo, BurgerIcon, ListIcon, ProfileIcon} from '@ya.praktikum/react-developer-burger-ui-components';
+import './app-header.scss';
+function AppHeader() {
+
+  const AppHeaderButton = ({typeButton, children, text}) => {
+    let typeOfButton = typeButton === 'active' ? '' : 'app-header__button_inactive';
+    
+    return(
+    <>
+      <button className={`app-header__button text text_type_main-default mt-4 mb-4 pl-5 pr-5 pb-4 pt-4 ${typeOfButton}`}>
+        <span className="mr-2">{children}</span> {text}
+      </button>
+    </>);
+  }
+
+  const NavLink = () =>{
+    
+    return(
+      <nav className='app-header__nav'>
+      <ul className=''>
+        <li>
+          <AppHeaderButton typeButton='active' text='Конструктор'>
+            <BurgerIcon type="primary" />
+          </AppHeaderButton>
+        </li>
+        <li>
+        <AppHeaderButton typeButton='' text='Лента заказов'>
+          <ListIcon type="secondary" />
+        </AppHeaderButton>
+        </li>
+      </ul>
+      </nav>
+    );
+  }
+
+
+  return(
+    <header className='app-header'>
+      <NavLink text='Текст' />
+      <Logo />
+      <AppHeaderButton typeButton='' text='Личный кабинет'>
+          <ProfileIcon type="secondary" />
+        </AppHeaderButton>
+    </header>
+  );
+}
+
+export default AppHeader; 
